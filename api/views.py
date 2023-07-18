0f19b60efed5744cf5521d346e1057fb77c8c680
Wojciech Wnek <wojciech.wnek97@gmail.com>
2023-07-18 16:37:50 +0200

    Make API restful

--- api/views.py
+++ api/views.py
@@ -1,13 +1,18 @@
+from django.http import response
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.serializers import Serializer
 from .models import Note
 from .serializers import NoteSerializer
-
+from api import serializers
+from .utils import updateNote, getNoteDetail, deleteNote, getNotesList, createNote
 # Create your views here.
+
+
 @api_view(['GET'])
 def getRoutes(request):
+
     routes = [
         {
             'Endpoint': '/notes/',
@@ -42,43 +47,24 @@ def getRoutes(request):
     ]
     return Response(routes)
 
-@api_view(["GET"])
+@api_view(['GET', 'POST'])
 def getNotes(request):
-    notes = Note.objects.all()
-    serializer = NoteSerializer(notes, many=True)
 
-    return Response(serializer.data)
+    if request.method == 'GET':
+        return getNotesList(request)
 
-@api_view(["GET"])
+    if request.method == 'POST':
+        return createNote(request)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def getNote(request, pk):
-    note = Note.objects.get(id=pk)
-    serializer = NoteSerializer(note, many=False)
 
-    return Response(serializer.data)
+    if request.method == 'GET':
+        return getNoteDetail(request, pk)
 
-@api_view(["POST"])
-def createNote(request):
-    data = request.data
-    note = Note.objects.create(body=data['body'])
-    serializer = NoteSerializer(note, many=False)
-    return Response(serializer.data)
+    if request.method == 'PUT':
+        return updateNote(request, pk)
 
-
-@api_view(["PUT"])
-def updateNote(request, pk):
-    data = request.data
-    note = Note.objects.get(id=pk)
-    serializer = NoteSerializer(instance=note, many=False)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-@api_view(["DELETE"])
-def deleteNote(request, pk):
-    note = Note.objects.get(id=pk)
-    note.delete()
-    return Response("Note was deleted!")
-
-
+    if request.method == 'DELETE':
+        return deleteNote(request, pk)
